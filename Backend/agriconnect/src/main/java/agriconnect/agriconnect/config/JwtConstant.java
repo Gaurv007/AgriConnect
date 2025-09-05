a136e759487dfc5dfb3fa2feb76034e781d145c9
ADARSH DUBEY <139523805+adarshiiit0117@users.noreply.github.com>
2025-09-05 17:16:07 +0530

    file structure

--- Backend/agriconnect/src/main/java/agriconnect/agriconnect/config/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package agriconnect.agriconnect.config;
-
-public class JwtConstant {
-    public static final String JWT_HEADER = "Authorization";
-    public static final String SECRET_KEY = "N+KtKDaX6rCJ2k2okOsvmT/xQ1bd9Ho+aRW7x1Q28gRGIt+tsU28p9sZ6+3HPcLXtG+gjZTnJHwT7HgHK0jpsA==";
-}
--- Backend/agriconnect/src/main/java/agriconnect/agriconnect/model/PricingUnit.java
+++
@@ -1,6 +0,0 @@
-package agriconnect.agriconnect.model;
-
-public enum PricingUnit {
-    PER_HOUR,
-    PER_DAY
-}
